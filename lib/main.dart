672c46c281dd854498f45ded5df4248f9ca35c1a
Gulam Anas <g.anas786@gmail.com>
2022-02-17 12:44:35 +0530

    world time app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location.dart';
+import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+    // home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
